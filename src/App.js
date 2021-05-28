5698a228031d36e8bd5c96e17c60dd27764a8453
jgreen44 <jgreen44@asu.edu>
2021-05-28 13:30:43 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 // import './App.css';
-import {useState, useEffect} from 'react'
+import {useEffect, useState} from 'react'
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import AddTask from "./components/AddTask";
+import Footer from  "./components/Footer";
+import About from  "./components/About";
 
 
 function App() {
@@ -11,42 +14,89 @@ function App() {
     const [tasks, setTasks] = useState([])
 
     useEffect(() => {
-        const fetchTasks = async () => {
-            const res = await fetch('http:///localhost:5000/tasks')
-            const data = res.json()
-            console.log(data)
+        const getTasks = async () => {
+            const tasksFromServer = await fetchTasks()
+            setTasks(tasksFromServer)
         }
-
-        fetchTasks()
+        getTasks()
     }, [])
 
+    // Fetch tasks
+    const fetchTasks = async () => {
+        const res = await fetch('http:///localhost:5000/tasks')
+        return await res.json()
+    }
+
+    // Fetch task
+    const fetchTask = async (id) => {
+        const res = await fetch(`http:///localhost:5000/tasks/${id}`)
+        return await res.json()
+    }
+
     // Add Task
-    const addTask = (task) => {
-        const id = Math.floor(Math.random() * 10000 + 1)
-        const newTask = {id, ...task}
-        setTasks([...tasks, newTask])
+    const addTask = async (task) => {
+        const res = await fetch('http://localhost:5000/tasks', {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(task)
+        })
+
+        const data = await res.json()
+
+        setTasks([...tasks, data])
+        // const id = Math.floor(Math.random() * 10000 + 1)
+        // const newTask = {id, ...task}
+        // setTasks([...tasks, newTask])
     }
 
     // Delete Task
-    const deleteTask = (id) => {
+    const deleteTask = async (id) => {
+        await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'DELETE'
+        })
+
         setTasks(tasks.filter((task) => task.id !== id))
     }
 
     // Toggle Reminder
-    const toggleReminder = (id) => {
+    const toggleReminder = async (id) => {
+        const taskToToggle = await fetchTask(id)
+        const updatedTask = {...taskToToggle, reminder: !taskToToggle.reminder}
+
+        const res = await fetch(`http://localhost:5000/tasks/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedTask)
+        })
+
+        const data = await res.json()
+
         setTasks(
             tasks.map((task) =>
-                task.id === id ? { ...task, reminder:
-                        !task.reminder } : task))
+                task.id === id ? { ...task, reminder: data.reminder } : task))
     }
     return (
+        <Router>
         <div className="container">
             <Header onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask}  />
-            { showAddTask &&  <AddTask onAdd={addTask}/>}
-            {tasks.length > 0 ?
-                <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
-                : "You're done for the day!"}
+            <Route path={'/'} exact render={(props) => (
+                <>
+                    { showAddTask &&  <AddTask onAdd={addTask}/>}
+                    {tasks.length > 0 ? (
+                        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+                    ) : (
+                        "You're done for the day!"
+                    )}
+                </>
+            )} />
+            <Route path={'/about'} component={About} />
+            <Footer />
         </div>
+        </Router>
     );
 }
 
